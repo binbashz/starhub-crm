e00774abda8a46a69c541d551410b0385d1438bf
binbashz <5732@holbertonstudents.com>
2024-02-09 04:36:18 -0300

    add comments for lead sect

--- mycrm/lead/views.py
+++ mycrm/lead/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 
+from .forms import AddCommentForm
 from .models import Lead
 
 from client.models import Client
@@ -30,6 +31,12 @@ class LeadDetailView(DetailView):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = AddCommentForm()
+        
+        return context
     
     def get_queryset(self):
         queryset = super(LeadDetailView,self).get_queryset()
@@ -102,6 +109,23 @@ class LeadCreatedView(CreateView):
         return redirect(self.get_success_url())
 
 
+class AddCommentView(View):
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        
+        form = AddCommentForm(request.POST)
+        
+        if form.is_valid():
+            team = Team.objects.filter(created_by=self.request.user)[0]
+            comment = form.save(commit=False)
+            comment.team = team
+            comment.created_by = request.user
+            comment.lead_id = pk
+            comment.save()
+        
+        return redirect('leads:detail', pk=pk)
+
+
 class ConvertToClientView(View):
     def get(self,request, *args, **kwargs):
         pk = self.kwargs.get('pk')
@@ -123,23 +147,3 @@ class ConvertToClientView(View):
         messages.success(request, "The lead was converted to a client.")
         
         return redirect('leads:list')
-
-@login_required
-def converted_to_client(request, pk):
-     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
-     team = Team.objects.filter(created_by=request.user)[0]
-     
-     client = Client.objects.create(
-         name=lead.name,
-         email=lead.email,
-         description=lead.description,
-         created_by=request.user,
-         team = team,
-         )
-     
-     lead.converted_to_client = True
-     lead.save()
-     
-     messages.success(request, "The lead was converted to a client.")
-     
-     return redirect('leads:list')
