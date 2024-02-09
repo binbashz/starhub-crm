cba59dd8ae1f353b015c4d3707bafc717a70caac
binbashz <5732@holbertonstudents.com>
2024-02-09 03:19:18 -0300

    class based view

--- mycrm/lead/urls.py
+++ mycrm/lead/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.LeadDetailView.as_view(), name='detail'),
     path('<int:pk>/delete/', views.LeadDeleteView.as_view(), name='delete'),
     path('<int:pk>/edit/', views.LeadUpdateView.as_view(), name='edit'),
-    path('<int:pk>/convert/', views.converted_to_client, name='convert'),
+    path('<int:pk>/convert/', views.ConvertToClientView.as_view(), name='convert'),
     path('add-lead/', views.LeadCreatedView.as_view(), name='add_lead'),
 ]
--- mycrm/lead/views.py
+++ mycrm/lead/views.py
@@ -1,16 +1,11 @@
-from typing import Any
-from django.contrib.auth.decorators import login_required
-from django.db.models.base import Model as Model
-from django.db.models.query import QuerySet
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.utils.decorators import method_decorator
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 
-from .forms import AddLeadForm
 from .models import Lead
 
 from client.models import Client
@@ -61,31 +56,42 @@ class LeadDeleteView(DeleteView):
 class LeadUpdateView(UpdateView):
     model = Lead
     fields = ('name', 'email', 'description', 'priority', 'status',)
+    success_url = reverse_lazy('leads:list')
     
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit lead'
+        
+        return context
     
     def get_queryset(self):
         queryset = super(LeadUpdateView, self).get_queryset()
 
         return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
     
-    def get_success_url(self):
-        return reverse_lazy('leads:list')
-     
 
 class LeadCreatedView(CreateView):
     model = Lead
     fields = ('name', 'email', 'description', 'priority', 'status',)
+    success_url = reverse_lazy('leads:list')
     
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
     
-    def get_success_url(self):
-        return reverse_lazy('leads:list')
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = Team.objects.filter(created_by=self.request.user)[0]
+        context['team'] = team
+        context['title'] = 'Add lead'
+        
+        return context
+        
+        
     def form_valid(self, form):
         team = Team.objects.filter(created_by=self.request.user)[0]
         
@@ -96,6 +102,27 @@ class LeadCreatedView(CreateView):
         return redirect(self.get_success_url())
 
 
+class ConvertToClientView(View):
+    def get(self,request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        
+        lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+        team = Team.objects.filter(created_by=request.user)[0]
+        
+        client = Client.objects.create(
+            name=lead.name,
+            email=lead.email,
+            description=lead.description,
+            created_by=request.user,
+            team = team,
+         )
+        
+        lead.converted_to_client = True
+        lead.save()
+        
+        messages.success(request, "The lead was converted to a client.")
+        
+        return redirect('leads:list')
 
 @login_required
 def converted_to_client(request, pk):
