1045bd3917ab9a6278798a957b0ca7121d9e2174
binbashz <5732@holbertonstudents.com>
2024-02-12 13:43:43 -0300

    delte cache for fix in statics

--- mycrm/userprofile/views.py
+++ mycrm/userprofile/views.py
@@ -7,15 +7,13 @@ from team.models import Team
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        
-    if form.is_valid():
-        user = form.save()
-        Userprofile.objects.create(user=user)
-        team = Team.objects.create(name='Team name', created_by=request.user)
-        team.members.add(request.user)
-        team.save()
-        
-        return redirect('/log-in/')
+        if form.is_valid():
+            user = form.save()
+            Userprofile.objects.create(user=user)
+            team = Team.objects.create(name='Team name', created_by=request.user)
+            team.members.add(request.user)
+            team.save()
+            return redirect('/log-in/')
     else:
         form = UserCreationForm()
     
